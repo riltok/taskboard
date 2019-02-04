ae7bfb10431dddb090b39e64a148f765d33ed4a1
riltok <siwmaks@gmail.com>
2019-02-04 20:14:18 +0300

    first commit

---
+++ taskboard-server/src/main/java/com/taskboard/server/TaskboardServerApplication.java
@@ -0,0 +1,14 @@
+package com.taskboard.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskboardServerApplication.class, args);
+	}
+
+}
+
